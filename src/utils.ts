b6f26a53f343986b011374fa0c16040bb1de78bd
Stephen Lang <stephen.lang@grafana.com>
2024-08-05 17:47:11 +0100

    make offset more bulletproof

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,12 @@
 import { OffsetUnits } from './types';
 
 export const buildOffsetString = (offset: OffsetUnits) => {
-  if (Object.keys(offset).length === 0) {
-    // Default to minimum allowed offset
-    return '';
-  }
   // iterate through all units and build a string from them
-  let unitString = 'offset ';
+  let unitString = '';
   for (const [unit, value] of Object.entries(offset)) {
-    if (value !== 0) {
+    if (value > 0) {
       unitString += `${value}${unit}`;
     }
   }
-  return unitString;
+  return unitString.length ? `offset ${unitString}` : '';
 };
