f1675bafb807207371d51d4c5dfaabdd63c2eba4
Coleman Rollins <coleman.rollins@grafana.com>
2024-12-16 11:27:50 -0800

    [cr] use consistent ESNext notation

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,4 @@ import { promql } from './promql';
 import { Expression } from './expression';
 import { MatchingOperator } from './types';
 
-module.exports = {
-  promql,
-  Expression,
-  MatchingOperator,
-};
+export { promql, Expression, MatchingOperator };
