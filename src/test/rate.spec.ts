25bfeb854efa68c6e0b7f18822d62eca149885de
Cristian Greco <cristian@regolo.cc>
2024-12-18 16:19:34 +0100

    add simple test case

--- src/test/rate.spec.ts
+++ src/test/rate.spec.ts
@@ -7,7 +7,7 @@ describe('Functions: rate', () => {
       actual: () => promql.rate({ expr: 'foo{bar="baz"}', interval: '5m' }),
       expected: 'rate(foo{bar="baz"}[5m])',
     },
-  ])('Generate PromQL rate qyert: $expected', ({ actual, expected }) => {
+  ])('Generate PromQL rate query: $expected', ({ actual, expected }) => {
     expect(actual()).toStrictEqual(expected);
   });
 });
